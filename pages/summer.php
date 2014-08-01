9fafcd2e9fc7db3a0b1d8db8a9dc31c3e95bb794
slava.sh <shk.slava@gmail.com>
2014-08-01 13:47:48 +0300

    Change summer text

--- pages/summer.php
+++ pages/summer.php
@@ -1,3 +1,3 @@
 <div style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; width: 500px; height: 200px; text-align: center">
-    <h1>Сейчас лето</h1>
+    <h1>Летом занятий нет</h1>
 </div>
