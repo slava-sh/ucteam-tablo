5814166da7e901536c68ccbee388d34c73088acd
slava.sh <shk.slava@gmail.com>
2014-08-15 22:14:51 +0300

    Rewrite update.php

--- update.php
+++ update.php
@@ -1,20 +1,5 @@
 <?php
 
-function preg_matches($pattern, $subject, $set_order = false) {
-    $order = $set_order ? PREG_SET_ORDER : PREG_PATTERN_ORDER;
-    $ok = preg_match_all($pattern, $subject, $matches, $order);
-    return $ok !== 0 && $ok !== false ? $matches : false;
-}
-
-function make_empty_empty($items) {
-    foreach ($items as &$item) {
-        if ($item == '&nbsp;') {
-            $item = '';
-        }
-    }
-    return $items;
-}
-
 $lesson_name_map = array(
     'Математ'    => 'Математика',
     'Геометр'    => 'Геометрия',
@@ -32,76 +17,78 @@ $lesson_name_map = array(
     'ВсИстор'    => 'ВсИстория',
 );
 
+$auditoriums = array_map('strval', array(
+    104, 105, 106, 107, 109,
+    200, 201, 211, 212,
+    301, 302, 303, 304, 305, 306, 307, 308, 309, 310,
+    116, 117, 127, 219, 220, 221, 222, 311, 312, 313, 314, 315
+));
+
 $now = new DateTime();
-$elevens_gone = $now > new DateTime('23.05.2014 00:00:00');
+$elevens_gone = $now > new DateTime('23.05.2015 00:00:00');
+
+$day_of_week_id = array(
+    'понедельник' => 'Mon',
+    'вторник'     => 'Tue',
+    'среду'       => 'Wed',
+    'четверг'     => 'Thu',
+    'пятницу'     => 'Fri',
+    'субботу'     => 'Sat',
+);
+
+function preg_matches($pattern, $subject, $set_order = false) {
+    $order = $set_order ? PREG_SET_ORDER : PREG_PATTERN_ORDER;
+    $ok = preg_match_all($pattern, $subject, $matches, $order);
+    return $ok !== 0 && $ok !== false ? $matches : false;
+}
 
 $html = mb_convert_encoding(file_get_contents('http://lyceum.urfu.ru/study/tablo.php'), 'UTF-8', 'cp1251');
-file_put_contents($cache_file, $html, LOCK_EX);
-file_put_contents($cache_dir . '/' . $now->format('y.m.d_D') . '.html', $html, LOCK_EX);
+file_put_contents('cache/' . $now->format('y.m.d_D') . '.html', $html);
 
-$html = file_get_contents($cache_file);
+$html = html_entity_decode(str_replace('&nbsp;', ' ', $html));
 $html_matches = preg_matches('@<h1.*?>(?<header>[^<]*)</h1><table.*?>(?<table><tr>(?<thead>.*?)</tr>(?<tbody>.*?)</table>)@s', $html, true);
-$days = array();
 foreach ($html_matches as $match) {
     $day = array();
-    $day['header'] = split(',', $match['header'], 2)[0];
+    $day['day_of_week'] = preg_matches('@Расписание на (.+?),@', $match['header'])[1][0];
+    $day['id'] = $day_of_week_id[$day['day_of_week']];
 
-    $table = array();
-    $table['headers'] = make_empty_empty(preg_matches('@<th.*?>(.*?)</th>@s', $match['thead'])[1]);
-    array_shift($table['headers']);
+    echo "Processing {$day['id']}\n";
+
+    $day['table'] = array();
     $tbody_matches = preg_matches('@<tr.*?>(.*?)</tr>@s', $match['tbody'])[1];
-    foreach ($tbody_matches as $i => $row_match) {
+    foreach ($tbody_matches as $row_match) {
         $row = array();
-        $items = make_empty_empty(preg_matches('@<td.*?>(.*?)</td>@s', $row_match)[1]);
+        $items = preg_matches('@<td.*?>(.*?)</td>@s', $row_match)[1];
         $row['group'] = preg_replace('@(\d+)@', '\1 ', array_shift($items));
         if ($elevens_gone && $row['group'] == 11) {
             continue;
         }
-        $lesson_index = 0;
         foreach ($items as $item) {
-            $lesson_index += 1;
             $sublessons = preg_split('@<br.*?>@', $item);
             $lesson = array();
             foreach ($sublessons as $sublesson) {
-                $sublesson_match = preg_matches('@<span class=\'aud\'>(?<auditorium>.+?)</span>&nbsp;(?<name>.+)@s', $sublesson, true);
-                if ($sublesson_match) {
-                    $sublesson = array(
-                        'name'       => $sublesson_match[0]['name'],
-                        'auditorium' => $sublesson_match[0]['auditorium'],
-                    );
-                }
-                else {
-                    $sublesson = array(
-                        'name'       => $sublesson,
-                    );
-                }
+                $sublesson_match = preg_matches('@(<span class=\'aud\'>(?<auditorium>.+?)</span>)?\s*(?<name>.*)@s', $sublesson, true);
+                $sublesson = array(
+                    'name'       => trim($sublesson_match[0]['name']),
+                    'auditorium' => trim($sublesson_match[0]['auditorium']),
+                );
                 if (isset($lesson_name_map[$sublesson['name']])) {
                     $sublesson['name'] = $lesson_name_map[$sublesson['name']];
-                }
-                if (empty($sublesson['auditorium'])) {
-                    $sublesson['auditorium'] = '';
                 }
                 $lesson[] = $sublesson;
             }
             $row['lessons'][] = $lesson;
         }
-        $table['rows'][$i] = $row;
+        $day['table'][] = $row;
     }
-    $day['table'] = $table;
 
     $free_auditoriums = array();
     for ($lesson_i = 0; $lesson_i < 7; ++$lesson_i) {
-        $auditoriums = array_map('strval', array(
-            104, 106, 107, 109, 105,
-            200, 201, 211, 212,
-            301, 302, 303, 304, 305, 306, 307, 308, 309, 310,
-            116, 117, 127, 219, 220, 221, 222, 311, 312, 313, 314, 315
-        ));
         $is_free = array();
-        foreach ($auditoriums as $i) {
-            $is_free[$i] = true;
+        foreach ($auditoriums as $auditorium) {
+            $is_free[$auditorium] = true;
         }
-        foreach ($day['table']['rows'] as $row) {
+        foreach ($day['table'] as $row) {
             foreach ($row['lessons'][$lesson_i] as $sublesson) {
                 $is_free[$sublesson['auditorium']] = false;
             }
@@ -115,7 +102,5 @@ foreach ($html_matches as $match) {
     }
     $day['free_auditoriums'] = $free_auditoriums;
 
-    $days[] = $day;
+    file_put_contents("cache/last_{$day['id']}.json", json_encode($day));
 }
-
-echo json_encode($days);
