79446a05dcbe1271504485707cf63246234e6856
slava.sh <shk.slava@gmail.com>
2014-08-16 20:39:42 +0300

    Improve usability on small screens

--- config.php
+++ config.php
@@ -33,24 +33,12 @@ if (!empty($_POST)) {
         body {
             background-color: #f9f9f9;
         }
-        .panel-body {
-            margin-right: -8.33333333%;
-        }
         .form-group .row {
             margin: 0 0 10px 0;
         }
         .form-group .row > div {
             padding-left: 0;
             padding-right: 5px;
-        }
-        .form-group .repeat-group {
-            padding-bottom: 10px;
-        }
-        .form-group .repeat-group:last-child {
-            padding-bottom: 0;
-        }
-        .form-group .repeat-group .row {
-            margin-bottom: 5px;
         }
     </style>
     <script>
@@ -67,78 +55,68 @@ if (!empty($_POST)) {
 </head>
 <body ng-controller="ctrl">
     <div class="container">
-        <div class="row">
-            <div class="col-sm-6 col-sm-push-3">
-                <br>
+        <br>
+        <div class="panel panel-default">
+            <div class="panel-body">
                 <form role="form" method="post" action="<?= htmlspecialchars($_SERVER['REQUEST_URI']) ?>">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="form-group">
-                                <label>Заглушки</label>
-                                <div class="repeat-group" ng-repeat="item in config.specials">
-                                    <div class="row">
-                                        <div class="col-sm-5">
-                                            <input name="specials[{{ $index }}][from]" type="datetime" class="form-control" ng-model="item.from">
-                                        </div>
-                                        <div class="col-sm-5">
-                                            <input name="specials[{{ $index }}][until]" type="datetime" class="form-control" ng-model="item.until">
-                                        </div>
-                                        <div class="col-sm-1">
-                                            <button type="button" class="btn btn-default" ng-click="config.specials.splice($index, 1)"><i class="fa fa-trash-o"></i></button>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-10">
-                                            <input name="specials[{{ $index }}][message]" type="text" class="form-control" ng-model="item.message">
-                                        </div>
-                                        <div class="col-sm-1">
-                                            <button type="button" class="btn btn-default" ng-click="config.specials.splice($index + 1, 0, { from: '<?= date('Y-m-d\T00:00:00\Z') ?>', until: '<?= date('Y-m-d\T23:59:59\Z') ?>' })"><i class="fa fa-plus"></i></button>
-                                        </div>
-                                    </div>
-                                </div>
+                    <div class="form-group">
+                        <label>Заглушки</label>
+                        <div class="row" ng-repeat="item in config.specials">
+                            <div class="col-xs-5 col-sm-2">
+                                <input name="specials[{{ $index }}][from]" type="datetime" class="form-control" ng-model="item.from">
                             </div>
-
-                            <div class="form-group">
-                                <label for="no_elevens">Когда перестать показывать 11 классы?</label>
-                                <div class="row">
-                                    <div class="col-sm-5">
-                                        <input name="no_elevens" id="no_elevens" type="datetime" class="form-control" ng-model="config.no_elevens">
-                                    </div>
-                                </div>
+                            <div class="col-xs-5 col-sm-2">
+                                <input name="specials[{{ $index }}][until]" type="datetime" class="form-control" ng-model="item.until">
                             </div>
-
-                            <div class="form-group">
-                                <label>Как изменять названия предметов?</label>
-                                <div class="row" ng-repeat="item in config.lesson_name_map">
-                                    <div class="col-sm-5">
-                                        <input name="lesson_name_map[{{ $index }}][from]" type="text" class="form-control" ng-model="item.from">
-                                    </div>
-                                    <div class="col-sm-5">
-                                        <input name="lesson_name_map[{{ $index }}][to]" type="text" class="form-control" ng-model="item.to">
-                                    </div>
-                                    <div class="col-sm-1">
-                                        <button type="button" class="btn btn-default" ng-click="config.lesson_name_map.splice($index, 1)"><i class="fa fa-trash-o"></i></button>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-5">
-                                        <button type="button" class="btn btn-default btn-block" ng-click="config.lesson_name_map.push({})"><i class="fa fa-plus"></i></button>
-                                    </div>
-                                </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <input name="specials[{{ $index }}][message]" type="text" class="form-control" ng-model="item.message">
                             </div>
-
-                            <div class="form-group">
-                                <label for="auditoriums">Все аудитории</label>
-                                <div class="row">
-                                    <div class="col-sm-11">
-                                        <textarea id="auditoriums" name="auditoriums" type="text" class="form-control" ng-bind="config.auditoriums.join(' ')"></textarea>
-                                    </div>
-                                </div>
+                            <div class="col-xs-12 col-sm-2">
+                                <button type="button" class="btn btn-default" ng-click="config.specials.splice($index, 1)"><i class="fa fa-trash-o"></i></button>
+                                <button type="button" class="btn btn-default" ng-click="config.specials.splice($index + 1, 0, { from: '<?= date('Y-m-d\T00:00:00\Z') ?>', until: '<?= date('Y-m-d\T23:59:59\Z') ?>' })"><i class="fa fa-plus"></i></button>
                             </div>
-
-                            <button type="submit" class="btn btn-primary">Сохранить</button>
                         </div>
                     </div>
+
+                    <div class="form-group">
+                        <label for="no_elevens">Когда перестать показывать 11 классы?</label>
+                        <div class="row">
+                            <div class="col-xs-5 col-sm-4 col-md-2">
+                                <input name="no_elevens" id="no_elevens" type="datetime" class="form-control" ng-model="config.no_elevens">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Как изменять названия предметов?</label>
+                        <div class="row" ng-repeat="item in config.lesson_name_map">
+                            <div class="col-xs-5 col-sm-4 col-md-2">
+                                <input name="lesson_name_map[{{ $index }}][from]" type="text" class="form-control" ng-model="item.from">
+                            </div>
+                            <div class="col-xs-5 col-sm-4 col-md-2">
+                                <input name="lesson_name_map[{{ $index }}][to]" type="text" class="form-control" ng-model="item.to">
+                            </div>
+                            <div class="col-xs-1">
+                                <button type="button" class="btn btn-default" ng-click="config.lesson_name_map.splice($index, 1)"><i class="fa fa-trash-o"></i></button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-5 col-sm-4 col-md-2">
+                                <button type="button" class="btn btn-default btn-block" ng-click="config.lesson_name_map.push({})"><i class="fa fa-plus"></i></button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="auditoriums">Все аудитории</label>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <textarea id="auditoriums" name="auditoriums" type="text" class="form-control" ng-bind="config.auditoriums.join(' ')"></textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Сохранить</button>
                 </form>
             </div>
         </div>
